a7fd98aa3422682738954e46cf08c3127d688a9e
Matheus Amim Martins de Almeida <124313377+Darks-049@users.noreply.github.com>
2024-09-10 09:42:37 -0300

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,31 @@
+﻿function buscarMago() {
+    var nomeDaCarta = document.getElementById("cartaInput").value;
+    fetch(`https://place.dog/300/200 ${nomeDoMago}`)
+      .then(response => response.json())
+      .then(data => mostrarMago(data))
+      .catch(error => console.error('Erro:', error));
+  }
+  
+  function mostrarMago(data) {
+    var cartasContainer = document.getElementById("cartasContainer");
+    cartasContainer.innerHTML = "";
+  
+    if (data.data.length === 0) {
+      cartasContainer.innerHTML = "<p>Nenhuma carta encontrada.</p>";
+      return;
+    }
+  
+    data.data.forEach(carta => {
+      var cartaDiv = document.createElement("div");
+      cartaDiv.classList.add("carta");
+  
+      var imagem = document.createElement("img");
+      imagem.src = carta.images.small;
+      imagem.alt = carta.name;
+  
+      cartaDiv.appendChild(imagem);
+  
+      cartasContainer.appendChild(cartaDiv);
+    });
+}
+
